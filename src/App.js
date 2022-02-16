5ee3d86219884044642b8917ffa202790e874142
Rox <88105134+heyroxette@users.noreply.github.com>
2022-02-16 13:12:02 -0500

    Updated App.js & App.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        Dictionary
+        <div className="mt-3">
+          <a href="#" className="btn btn-primary shadow">
+            Search
+          </a>
+        </div>
       </header>
     </div>
   );
