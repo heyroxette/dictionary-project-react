3f13270697618dbd33231e8d12f381509b017b58
Rox <88105134+heyroxette@users.noreply.github.com>
2022-02-16 18:50:41 -0500

    Added api documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,10 +11,11 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-  }
 
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
