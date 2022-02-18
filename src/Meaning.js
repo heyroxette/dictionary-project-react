443705ab7841b41dcaa2210f90380aaacee0a238
Rox <88105134+heyroxette@users.noreply.github.com>
2022-02-17 22:36:34 -0500

    taken off margin top

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ import Synonyms from "./Synonyms";
 export default function Meaning (props) {
   
   return (
-    <div className="Meaning mt-5">
+    <div className="Meaning">
       <h3> {props.meaning.partOfSpeech} </h3>
       {props.meaning.definitions.map(function(definition, index) {
         return (
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ export default function Synonyms(props) {
 
   if (props.synonyms) {
     return (
-      <ul className="Synonyms mt-3">
+      <ul className="Synonyms">
       {props.synonyms.map(function (synonym, index) {
         return (
           <small>
